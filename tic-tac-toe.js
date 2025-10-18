7e3a94f1dce3df68812f4890acd0af27b6433bdc
khanezwal464 <khanezwallace18@gmail.com>
2025-10-18 01:12:17 -0500

    Completed Exercises 2 and 3

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,14 +1,66 @@
 //Waiting until the DOM is fully loaded 
 window.addEventListener("DOMContentLoaded", () => {
 
-    //Selecting the game board
-    const gameboard = document.getElementById("gameboard");
+    //Constant Declarations
+    const board = document.getElementById("board");
+    const squares = board.querySelectorAll("div");
+    const statusDiv = document.getElementById("status");
+    const newGameBtn = document.querySelector(".btn");
 
-    //Getting all squares inside the board
-    const squares = gameboard.querySelectorAll("div");
+    //Variables for the game state
 
-    //Adding the CSS class "square" to each div for styling 
-    squares.forEach(sq => {
-        sq.classList.add("sq");
+    //Starting with X
+    let currPlayer = "X"; 
+    //Tracking board state
+    let gameState = Array(9).fill(null);
+    let gameOver = false;
+
+    //Styling of board for Exercise 1
+    squares.forEach(square => {
+        square.classList.add("square");
     });
+
+    //Adding click event to each square
+    squares.forEach((square,index) => {
+        square.addEventListener("click", () => {
+            //Prevent overwriting a square
+            if (gameState[index] !== null) return;
+
+            //Mark the square 
+            square.textContent = currPlayer;
+            square.classList.add(currPlayer);
+            //Prevent overwriting a square
+            if (gameState[index] !== null) return;
+
+            // Mark the square 
+            square.textContent = currPlayer;
+            square.classList.add(currPlayer);
+            gameState[index] = currPlayer;
+
+            //Switch player turn
+            currPlayer = currPlayer == "X" ? "O" : "X";
+        });
+
+        //Changing the style when mouse hovers ovber a square
+        square.addEventListener("mouseover", () => {
+            if(!gameOver && !square.textContent) {
+                square.classList.add("hover");
+            }
+        });
+
+        square.addEventListener("mouseout", () => {
+            square.classList.add("hover");
+
+        });
+        //Restarting game
+        //newGameBtn
+
+    });
+
+    //Checking for winner
+    //function checkWin() {
+        //const winningMoves = [
+
+        //]
+    //}
 });
